d1fd04cd293c21a2ee59cdcbcdd903a36d66844e
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2024-01-21 15:02:51 +0500

    upppp[

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Navbar from "./views/header";
+import Navbar from "../components/header";
 
 
 const inter = Inter({ subsets: ["latin"] });
