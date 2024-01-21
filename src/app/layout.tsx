f77135f0f07938b48af23d84fe8bcc7e3d278e28
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2024-01-21 15:14:55 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Navbar from "../components/header";
-
+import Card from "@/components/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,9 @@ function Layout ({children}:{children:React.ReactNode}){
       return <html lang="en">
       <body className={inter.className}>
         <Navbar/>
+        <Card/>
         {children}
+       
       </body>
     </html>
 }
